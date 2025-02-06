11d4ff7a9b6acfbdaaf864c051927da764618070
Victor Ramirez <vicdeveloper83@gmail.com>
2025-02-06 22:23:20 +0000

    prisma client created

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
 import Server from './server';
 import { envs } from './plugins/envs/envs.plugin';
 import { app } from './server';
@@ -11,7 +12,10 @@ envs.NODE_ENV === 'development' && app.use(morgan('dev'));
 app.use(express.json());
 
 //*Middlewares===============
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(cookieParser());
 //?Routes====================
 
 //?Routes====================
